0c5f9512b837605b144137a95c55f31686a41b5a
Jave <david.jaeggli@gmail.com>
2023-04-28 13:22:04 +0200

    Project restructuring

---
+++ build/src/CMakeFiles/pw_manager.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/dave/Documents/dev/vscode/c/i2c_project_code/src/crypto.c" "src/CMakeFiles/pw_manager.dir/crypto.c.o" "gcc" "src/CMakeFiles/pw_manager.dir/crypto.c.o.d"
+  "/home/dave/Documents/dev/vscode/c/i2c_project_code/src/files.c" "src/CMakeFiles/pw_manager.dir/files.c.o" "gcc" "src/CMakeFiles/pw_manager.dir/files.c.o.d"
+  "/home/dave/Documents/dev/vscode/c/i2c_project_code/src/main.c" "src/CMakeFiles/pw_manager.dir/main.c.o" "gcc" "src/CMakeFiles/pw_manager.dir/main.c.o.d"
+  "/home/dave/Documents/dev/vscode/c/i2c_project_code/src/password.c" "src/CMakeFiles/pw_manager.dir/password.c.o" "gcc" "src/CMakeFiles/pw_manager.dir/password.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/pw_manager.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pw_manager.dir/crypto.c.o"
+  "CMakeFiles/pw_manager.dir/crypto.c.o.d"
+  "CMakeFiles/pw_manager.dir/files.c.o"
+  "CMakeFiles/pw_manager.dir/files.c.o.d"
+  "CMakeFiles/pw_manager.dir/main.c.o"
+  "CMakeFiles/pw_manager.dir/main.c.o.d"
+  "CMakeFiles/pw_manager.dir/password.c.o"
+  "CMakeFiles/pw_manager.dir/password.c.o.d"
+  "pw_manager"
+  "pw_manager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pw_manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
