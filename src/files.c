594ccd81c7ce40c53d6301b803891086cea989f4
Jave01 <david.jaeggli@gmail.com>
2023-05-08 13:45:31 +0200

    fix remove entry only removing value

--- src/files.c
+++ src/files.c
@@ -175,7 +175,7 @@ bool set_entry(pw_list_t* pwList, const unsigned char *key, const unsigned char 
     entry_value = calloc(MAX_VAL_LEN, sizeof(unsigned char));
     if(entry_value == NULL){
         printf("\033[31m"); // set text color to red
-        perror("[!] Error while allocating memory");
+        printf("[!] Error while allocating memory");
         printf("\033[0m"); // reset text color to default
         return false;
     }
@@ -212,15 +212,15 @@ bool set_entry(pw_list_t* pwList, const unsigned char *key, const unsigned char 
 
 
 bool remove_entry(pw_list_t* pwList, const unsigned char *key){
-    unsigned char* entry_value = 0;
-    entry_value = calloc(MAX_VAL_LEN, sizeof(unsigned char));
-    if(entry_value == NULL){
-        printf("\033[31m"); // set text color to red
-        perror("[!] Error while allocating memory");
-        printf("\033[0m"); // reset text color to default
-        return false;
-    }
-    long entry_offset = get_entry_value(pwList, entry_value, key);
+    // unsigned char* entry_value = 0;
+    // entry_value = calloc(MAX_VAL_LEN, sizeof(unsigned char));
+    // if(entry_value == NULL){
+    //     printf("\033[31m"); // set text color to red
+    //     printf("[!] Error while allocating memory");
+    //     printf("\033[0m"); // reset text color to default
+    //     return false;
+    // }
+    long entry_offset = search_entry(pwList, key);
     if (entry_offset >= 0)
     {
         unsigned char* new_entries = calloc(pwList->size * ENTRIES_BLOCK_SIZE, sizeof(unsigned char));
@@ -229,7 +229,8 @@ bool remove_entry(pw_list_t* pwList, const unsigned char *key){
         strncpy(new_entries, pwList->entries, entry_offset);
 
         // copy values after old value
-        strcat(new_entries, pwList->entries + entry_offset + strlen(entry_value) + 1);
+        int entry_len = (unsigned char*)strchr(pwList->entries + entry_offset, '\n') - (pwList->entries + entry_offset); 
+        strcat(new_entries, pwList->entries + entry_offset + entry_len + 1);
 
         strncpy(pwList->entries, new_entries, pwList->size * ENTRIES_BLOCK_SIZE);
         save_to_file(pwList);
